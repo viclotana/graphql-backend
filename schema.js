65d95f21b33faa9e70766d4cddeb813ad28efc23
viclotana <viclotana@gmail.com>
2018-08-02 20:13:09 +0100

    up2

--- schema.js
+++ schema.js
@@ -7,10 +7,13 @@ const {
     GraphQLNonNull
 } = require('graphql');
 
+
+// Customer type
+const CustomerType
 //root query
 const RootQuery = new GraphQLObjectType({
     name:'RootQueryType',
-    customer: CuustomerType
+    customer: CustomerType
 });
 
 module.exports = new GraphQLSchema({
