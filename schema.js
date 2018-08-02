ebcf3ecf769757331766b19c9ea07fe3541dd738
viclotana <viclotana@gmail.com>
2018-08-02 19:53:47 +0100

    first commit

---
+++ schema.js
@@ -0,0 +1,13 @@
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLLint,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull
+} = require('graphql');
+
+
+Module.exports = new GraphQLSchema({
+
+});
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema.js');
+
+const app = express();
+
+app.use('graphql', expressGraphQL({
+    schema:schema,
+    graphiql: true
+}));
+app.listen(4100, () =>{
+    console.log('server running on port 4100...')
+});
