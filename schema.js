55651ceec37cd1d28fc163a012e70100e64d88f2
viclotana <viclotana@gmail.com>
2018-08-03 20:20:21 +0100

    updating updates mutation

--- schema.js
+++ schema.js
@@ -83,6 +83,23 @@ const mutation = new GraphQLObjectType({
                 return axios.delete('http://localhost:3000/customers/'+args.id)
                 .then(res => res.data);
             }
+        },
+        updateCustomer:{
+            type:CustomerType,
+            args: {
+                id: {type: new GraphQLNonNull(GraphQLString)}
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                email: {type: new GraphQLNonNull(GraphQLString)},
+                age: {type: new GraphQLNonNull(GraphQLInt)},
+            },
+            resolve(parentValue, args){
+                return axios.post('http://localhost:3000/customers',{
+                    name:args.name,
+                    email:args.email,
+                    age:args.age
+                })
+                .then(res => res.data);
+            }
         }
     }
 })
