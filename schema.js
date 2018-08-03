3dc14bfa3ec4e9ac341062df00bfcf25e808b9bb
viclotana <viclotana@gmail.com>
2018-08-03 19:50:11 +0100

    axios updates

--- schema.js
+++ schema.js
@@ -48,7 +48,8 @@ const RootQuery = new GraphQLObjectType({
         customers:{
             type: new GraphQLList(CustomerType),
             resolve(parentValue, args){
-                return customers;
+                return axios.get("http://localhost:3000/customers")
+                .then(res => res.data); 
             }
         }
     }
