53cd3943fada677dd935817736fd1abca10285b2
viclotana <viclotana@gmail.com>
2018-08-03 14:09:32 +0100

    removing hardcoded data

--- schema.js
+++ schema.js
@@ -8,11 +8,11 @@ const {
 } = require('graphql');
 
 // hardcoded data
-const customers = [
-    {id:'1',name:'Nwose Lotanna', email:'lnwose@gmail.com', age:24},
-    {id:'2',name:'Jeff Bezos', email:'jeffb@gmail.com', age:54},
-    {id:'3',name:'Ebube Gates', email:'egatese@gmail.com', age:64}
-]
+// const customers = [
+//     {id:'1',name:'Nwose Lotanna', email:'lnwose@gmail.com', age:24},
+//     {id:'2',name:'Jeff Bezos', email:'jeffb@gmail.com', age:54},
+//     {id:'3',name:'Ebube Gates', email:'egatese@gmail.com', age:64}
+// ]
 
 // Customer type
 const CustomerType = new GraphQLObjectType({
@@ -34,11 +34,11 @@ const RootQuery = new GraphQLObjectType({
                 id:{type:GraphQLString}
             },
             resolve(parentValue, args){
-                for(let i=0;i<customers.length;i++){
-                    if(customers[i].id==args.id){
-                        return customers[i];
-                    }
-                }
+                // for(let i=0;i<customers.length;i++){
+                //     if(customers[i].id==args.id){
+                //         return customers[i];
+                //     }
+                // }
             }
         },
         customers:{
