c57be1602e854466d7cc787dc21fc886f53a9f56
viclotana <viclotana@gmail.com>
2018-08-03 13:25:09 +0100

    updates

--- schema.js
+++ schema.js
@@ -40,6 +40,12 @@ const RootQuery = new GraphQLObjectType({
                     }
                 }
             }
+        },
+        customers:{
+            type: new GraphQLList(CustomerType),
+            resolve(){
+                
+            }
         }
     }
 });
